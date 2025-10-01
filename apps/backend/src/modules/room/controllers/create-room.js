3bb649eddd1af92e6d9e682353a1ee9256686908
bommer153 <menthol.esports04@gmail.com>
2025-10-01 22:53:54 +0800

    feat : use crypto for random geneator

--- apps/backend/src/modules/room/controllers/create-room.js
+++ apps/backend/src/modules/room/controllers/create-room.js
@@ -22,11 +22,16 @@ export async function createRoom(req, res) {
   }
 }
 
-function generateRoomCode(length = 4) {
+function generateRoomCode(length = 6) {
   const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
   let code = '';
+
+
+  const randomValues = crypto.randomBytes(length);
+
   for (let i = 0; i < length; i++) {
-    code += chars.charAt(Math.floor(Math.random() * chars.length));
+    code += chars[randomValues[i] % chars.length];
   }
+
   return code;
 }
