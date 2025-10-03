79a2440cfe65d9b7619bbf69fa2fdab051be4b66
bommer153 <menthol.esports04@gmail.com>
2025-10-03 19:36:21 +0800

    fix : card verification

--- apps/backend/src/modules/card/controllers/verify-card.js
+++ apps/backend/src/modules/card/controllers/verify-card.js
@@ -1,17 +1,20 @@
 import { Card } from '#modules/card/models/card.js';
-import { Room } from '#modules/room/models/card.js';
+import { Room } from '#modules/room/models/room.js';
 import { log } from '#utils/log.js';
 
 export async function verifyCard(req, res) {
   try {
     const { roomCode, cardId } = req.body;
+    let isWinner = false;
 
     const room = await Room.findOne({ code: roomCode });
     const card = await Card.findById(cardId);
     const drawnNumber = room.drawnNumber;
     const trueNumber = card.winningNumber;
 
-    const isWinner = trueNumber.every((num) => drawnNumber.includes(num));
+    if (trueNumber.length == 9) {
+      isWinner = trueNumber.every((num) => drawnNumber.includes(num));
+    }
 
     return res.status(201).json({
       message: 'Successfully Verify Card',
